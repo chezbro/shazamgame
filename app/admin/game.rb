cd0da6ac661237954d7aa8b3da71fb5c99353ae8
Prelang Builder <builder@prelang.com>
2016-07-21 17:03:17 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Game' to allow for create, update

--- app/admin/game.rb
+++ app/admin/game.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Game do
+  permit_params :points, :is_home_team, :spread, :home_team_id, :away_team_id, :home_team_pref_pick, :away_team_pref_pick, :home_team_spread_pick, :away_team_spread_pick, :home_team_covered_spread, :away_team_covered_spread, :tie_game, :game_selected_by_admin
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
