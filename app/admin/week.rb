b0aca440fbf9458f606bf312b40c78ade1c6ae09
Prelang Builder <builder@prelang.com>
2016-07-21 17:03:30 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Week' to allow for create, update

--- app/admin/week.rb
+++ app/admin/week.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Week do
+  permit_params :week_number, :year, :year_in_datetime
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
