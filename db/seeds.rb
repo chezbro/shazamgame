dc166deaf90735854f48759d23f1760dc233f25e
EMC <ericchesbrough@gmail.com>
2016-08-15 12:03:39 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin_user1@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin_user@example.com', password: 'password', password_confirmation: 'password')
 
 case Rails.env
 when "development"
@@ -24,6 +24,13 @@ when "development"
 
 when "production"
 
+  Week.create(week_number: "1", year: "2016", active: true)
+
+  User.create(email: "admin@example.com", username: "admin", password: "password", password_confirmation: "password", address: "123 Admin Street", fav_teams: "Chicago Cubs, Chicago Bears", cell_phone_number: "312-123-2345", admin: true)  
+
+  User.create(email: "eric@example.com", username: "eric", password: "password", password_confirmation: "password", address: "123 Eric Street", fav_teams: "Detroit Lions, Detroit Tigers", cell_phone_number: "248-123-2345", admin: false)
+
+  User.create(email: "chezbro@example.com", username: "chezbro", password: "password", password_confirmation: "password", address: "123 Chezbro Street", fav_teams: "Detroit Pistons, Detroit Red Wings", cell_phone_number: "313-123-2345", admin: false)
 
 
 
