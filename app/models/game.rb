c9de59c4107f842a658e240dc3cdeb4a3014832e
EMC <ericchesbrough@gmail.com>
2016-09-01 11:17:34 -0500

    point distribution seems to be working smoothly

--- app/models/game.rb
+++ app/models/game.rb
@@ -60,8 +60,9 @@ def which_team_covered_spread
 
   elsif ( ( (self.home_team_score + self.spread ) - self.away_team_score ) == 0 )
     self.tie_game = true
-  # Away Team Covered
+    self.team_that_covered_spread = nil
   else
+  # Away Team Covered
     self.team_that_covered_spread = self.away_team.id
   end
     self.save!
@@ -73,32 +74,33 @@ def tally_points
   User.all.each do |user|
     user.selections.each do |selection|
       if selection.game_id == self.id && selection.game.week.id == Week.last.id
-        if selection.spread_pick_team == self.team_that_won_straight_up
+        if selection.spread_pick_team == self.team_that_covered_spread
           user.weekly_points += 7
           user.weekly_points_game_b += 7
           user.cumulative_points += 7
           user.save!
-        else       
-          user.weekly_points += 0
-          user.weekly_points_game_b += 0
-          user.cumulative_points += 0
-          user.save!
+        # else       
+        #   user.weekly_points += 0
+        #   user.weekly_points_game_b += 0
+        #   user.cumulative_points += 0
+        #   user.save!
         end
-        if selection.pref_pick_team == self.team_that_covered_spread
+        if selection.pref_pick_team == self.team_that_won_straight_up
           user.weekly_points += selection.pref_pick_int
           user.weekly_points_game_a += selection.pref_pick_int
           user.cumulative_points += selection.pref_pick_int
           user.save!
-        else
-          user.weekly_points += 0
-          user.weekly_points_game_a += 0
-          user.cumulative_points += 0
-          user.save!
+        # else
+        #   user.weekly_points += 0
+        #   user.weekly_points_game_a += 0
+        #   user.cumulative_points += 0
+        #   user.save!
         end
-        # Does this work? It's not hitting the binding here.
         if selection.game.tie_game == true
           user.weekly_points += 7
-          user.weekly_points_game_a += 7
+          user.weekly_points_game_b += 7
+          user.cumulative_points += 7
+          user.save!
         end
       end
     end
