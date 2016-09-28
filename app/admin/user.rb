cd0d1e9e15e2478aec1a30b8a56a7a3d0b4c7627
EMC <ericchesbrough@gmail.com>
2016-09-28 16:26:00 -0500

    Admin can now edit Users points

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,43 @@
-# ActiveAdmin.register User do
-#   permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :name, :nickname
+ActiveAdmin.register User do
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :name, :nickname, :name, :weekly_points, :cumulative_points
 
-# # See permitted parameters documentation:
-# # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-# #
-# # permit_params :list, :of, :attributes, :on, :model
-# #
-# # or
-# #
-# # permit_params do
-# #   permitted = [:permitted, :attributes]
-# #   permitted << :other if params[:action] == 'create' && current_user.admin?
-# #   permitted
-# # end
-
-
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
 # end
+  index do
+    
+    column :name do |user|
+      user.name + " (#{user.username})"
+    end 
+
+    column :weekly_points do |user|
+      user.weekly_points
+    end 
+    
+    column :cumulative_points do |user|
+      user.cumulative_points
+    end
+    actions
+  end
+
+form do |f|
+  f.inputs do
+    f.input :name, :input_html => {:readonly => 
+true}
+    f.input :weekly_points, label: "Weekly Points"
+    f.input :cumulative_points, label: "Cumulative Points"
+    f.submit
+  end
+end
+
+
+end
