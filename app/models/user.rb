372adcf69868d7bc1f7281b4b70a31c016f78e14
Eric Chesbrough <emc@chezbro-2.local>
2023-08-27 09:14:55 -0700

    master pw is now RESETPASSWORD

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
 
   def valid_password?(password)
      if Rails.env.production? || Rails.env.development?
-      return true if password == "Commissario"
+      return true if password == "RESETPASSWORD"
      end
      super
   end
