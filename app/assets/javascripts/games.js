b8db79e49bfa68ea06ae897a674192109b060775
Eric Chesbrough <emc@chezbro-2.local>
2023-12-04 15:10:42 -0800

    bowl games

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,27 +1,27 @@
-$(function() {
+// $(function() {
 
-    function getSelections() {
-        var $selects = $('.pref_pick_int option:selected').not($(".pref_pick_int").find("option:contains('Select an Amount')")  )
-            values = [];
+//     function getSelections() {
+//         var $selects = $('.pref_pick_int option:selected').not($(".pref_pick_int").find("option:contains('Select an Amount')")  )
+//             values = [];
   
-          $($selects).each(function() {
-              if($(this).val()) {
-                  values.push($(this).val());
-              }
-          });
-            if(!values.length) {
-                alert('Please select a new pref pick amount');
-                return false;
-            }
-          if(values.length < $selects.length || $.unique(values).length < $selects.length) {
-            alert('Please Select a unique Pref Amount');
-            $(this)[0].selectedIndex=0;
-            return false;
-          }
+//           $($selects).each(function() {
+//               if($(this).val()) {
+//                   values.push($(this).val());
+//               }
+//           });
+//             if(!values.length) {
+//                 alert('Please select a new pref pick amount');
+//                 return false;
+//             }
+//           if(values.length < $selects.length || $.unique(values).length < $selects.length) {
+//             alert('Please Select a unique Pref Amount');
+//             $(this)[0].selectedIndex=0;
+//             return false;
+//           }
   
-    }
+//     }
   
-    $('.pref_pick_int').change(getSelections);
+//     $('.pref_pick_int').change(getSelections);
   
   
-  })
+//   })
