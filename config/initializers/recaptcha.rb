eb36e118c6e145de05b632982f7c8247689a3627
ckcimd@aol.com <ericchesbrough@gmail.com>
2024-08-16 09:41:58 -0700

    recaptcha2

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-    config.public_key  = '6LeKWygqAAAAAPmOliF0Gv3ASXHhZwx6v4cHEA9Y'
-    config.private_key = '6LeKWygqAAAAAGhfpDOYvXMOBTwZhvOpXgsIW2OX'
+    config.public_key  = 6LeKWygqAAAAAPmOliF0Gv3ASXHhZwx6v4cHEA9Y
+    config.private_key = 6LeKWygqAAAAAGhfpDOYvXMOBTwZhvOpXgsIW2OX
   end
