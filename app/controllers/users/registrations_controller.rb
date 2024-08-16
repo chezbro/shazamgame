18031919c367d7442ea44f49d8068186d52efc44
ckcimd@aol.com <ericchesbrough@gmail.com>
2024-08-16 09:48:36 -0700

    recaptcha3

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # http_basic_authenticate_with :name => "shazam2019", :password => "shazam2019", only: :new
-
   include ApplicationHelper
+  prepend_before_action :check_captcha, only: [:create]
 
   def create
     def create
@@ -25,3 +25,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 end
+
+private
+
+def check_captcha
+    return if verify_recaptcha # verify_recaptcha(action: 'signup') for v3
+
+    self.resource = resource_class.new sign_up_params
+    resource.validate # Look for any other validation errors besides reCAPTCHA
+    set_minimum_password_length
+
+    respond_with_navigational(resource) do
+     flash.discard(:recaptcha_error) # We need to discard flash to avoid showing it on the next page reload
+      render :new
+    end
+  end
--- config/initializers/recaptcha.rb
+++
@@ -1,4 +0,0 @@
-Recaptcha.configure do |config|
-    config.public_key  = 6LeKWygqAAAAAPmOliF0Gv3ASXHhZwx6v4cHEA9Y
-    config.private_key = 6LeKWygqAAAAAGhfpDOYvXMOBTwZhvOpXgsIW2OX
-  end
