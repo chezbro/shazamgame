2db5d698cba0ac70c4d2ee4cc78e6abaa7dcde88
ckcimd@aol.com <ericchesbrough@gmail.com>
2024-08-27 13:12:35 -0700

    commented out recaptcha stuff

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,21 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # http_basic_authenticate_with :name => "shazam2019", :password => "shazam2019", only: :new
+
   include ApplicationHelper
-  prepend_before_action :check_captcha, only: [:create]
 
-
-    def create
-      if verify_recaptcha
-        super
-      else
-        build_resource(sign_up_params)
-        clean_up_passwords(resource)
-        flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
-        flash.delete :recaptcha_error
-        render :new
-      end
-    end
-
+  def create
+    super
+  end
 
   def new
     super
@@ -24,14 +14,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def edit
     super
   end
-
-  private
-  def check_captcha
-    unless verify_recaptcha
-      self.resource = resource_class.new sign_up_params
-      respond_with_navigational(resource) { redirect_back }
-    end 
-  end
-
-
-  end
+end
