e3496d35838a967b3b53f941fe191aa3736e682f
ckcimd@aol.com <ericchesbrough@gmail.com>
2025-01-06 19:12:43 -0800

    2 games for semi finals

---
+++ app/assets/javascripts/active_admin.js
@@ -0,0 +1,6 @@
+// Remove or comment out the existing code since it's no longer needed
+// $(document).ready(function() {
+//   $('.week-points-select').on('change', function() {
+//     ...
+//   });
+// }); 
